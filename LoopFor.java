af27bbd7c86e547c1bfc0f171dfa2e9430cd9cf0
issach315 <issachemani227@gmial.com>
2023-01-28 18:54:02 +0530

    for loop in java

---
+++ LoopFor.java
@@ -0,0 +1,15 @@
+package LoopsInJava;
+
+public class LoopFor 
+{
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 10; i++)
+         {
+
+            System.out.println("india");
+            
+        }
+    }
+    
+}
