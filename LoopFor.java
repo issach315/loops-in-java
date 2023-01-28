231c753f00405e58e8aa5776780375a719e9c032
issach315 <117745188+issach315@users.noreply.github.com>
2023-01-28 18:56:07 +0530

    telling which type of loop

--- LoopFor.java
+++ LoopFor.java
@@ -3,6 +3,8 @@ package LoopsInJava;
 public class LoopFor 
 {
     public static void main(String[] args) {
+        // for loop in java 
+        
         
         for (int i = 0; i < 10; i++)
          {
